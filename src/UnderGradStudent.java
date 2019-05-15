3e0468e4acb2606a5619d1b50ac71e5127c37eac
amberbrown4 <46957388+amberbrown4@users.noreply.github.com>
2019-05-15 18:44:03 +0430

    Update UnderGradStudent.java

--- src/UnderGradStudent.java
+++ src/UnderGradStudent.java
@@ -1,20 +1,11 @@
 public class UnderGradStudent extends Student{
 
-    private Type field;
-
-    public UnderGradStudent(String firstName, String lastName, String ID, int joiningYear, Department department, Type field) {
+    private int field =0;
+    public UnderGradStudent(String firstName, String lastName, String ID, int joiningYear, Department department, int field) {
         super(firstName, lastName, ID, joiningYear, department);
         this.field = field;
     }
 
-    public void setField(Type field) {
-        this.field = field;
-    }
-
-    public Type getField()
-    {
-        return field;
-    }
 }
 
-}
+
